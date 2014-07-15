4f83fe7b900cf13ac53cd6d5031f517066c10bf2
Vincent Chen <vince78718@gmail.com>
2014-07-15 13:45:53 +0800

    Initial Commit

---
+++ exitingThreads/VCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VCAppDelegate.h
+//  exitingThreads
+//
+//  Created by vincent on 2014/07/15.
+//  Copyright (c) 2014年 Vincent Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ exitingThreads/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  exitingThreads
+//
+//  Created by vincent on 2014/07/15.
+//  Copyright (c) 2014年 Vincent Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VCAppDelegate class]));
+    }
+}
